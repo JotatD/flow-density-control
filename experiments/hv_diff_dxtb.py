de78695e4f264799bf37ffffb34ba57cb11638a2
Juan Guevara <jdg8833@nyu.edu>
2026-07-16 20:31:49 +0400

    further tracking of hypervol

--- experiments/hv_diff_dxtb.py
+++ experiments/hv_diff_dxtb.py
@@ -80,10 +80,14 @@ def main(config: OmegaConf) -> None:
     
     md_step = log.set_step_metric(0, "md_step")
     global_step = log.set_step_metric(0, "global_step")
+    most_inner_step = log.set_step_metric(0, "most_inner_step")
+    
     n_hv = log.watch('n_hypervolume', 'md_step')
     full_hv = log.watch('full_hypervolume', 'md_step')
     obj_img = log.set_image('objective_points', 'md_step')
     inner_hv = log.watch('inner_hypervolume', 'global_step')
+    inner_loss = log.watch('inner_loss', 'most_inner_step')
+    inner_img = log.set_image('inner_objective_points', 'most_inner_step')
     
     hv_computer = HVComputer(ref_point=reward.ref_point, num_rew=reward.num_rew)
     
@@ -98,10 +102,17 @@ def main(config: OmegaConf) -> None:
             md_step += 1
             for am in range(config.adjoint_matching.num_iterations):
                 global_step += 1
+                
                 am_dataset = trainer.generate_dataset()
-                # dataset_rewards = torch.cat([d.rewards for d in am_dataset], dim=0)
-                # inner_hv.val = hv_computer(dataset_rewards.reshape(1, -1, trainer.num_rews)).detach().cpu().item()
-                loss.val = trainer.finetune(am_dataset, steps=None)
+                losses = trainer.finetune(am_dataset, steps=None, debug=True)
+                
+                loss.val = np.array(losses).mean().item()
+                dataset_rewards = torch.cat([d.rews for d in am_dataset], dim=0)
+                inner_hv.val = hv_computer(dataset_rewards.unsqueeze(0)).item()
+                inner_img.val = plot_objective_points(ambient=ambient, special=dataset_rewards)
+                for l in losses: 
+                    most_inner_step += 1
+                    inner_loss.val = l
             n_hv.val, full_hv.val, reward_values = evaluate_hypervolume(trainer, num_samples=vol_samples, hv_computer=hv_computer)
             obj_img.val = plot_objective_points(ambient=ambient, special=reward_values)
             
@@ -122,6 +133,7 @@ def main(config: OmegaConf) -> None:
         return full_hv.val
 
 def optuna_entry(trial: optuna.Trial) -> float:
+    x = 16
     args = parse_args()
     config = {
         "seed": 5,
@@ -131,17 +143,17 @@ def optuna_entry(trial: optuna.Trial) -> float:
         "lmbda": trial.suggest_categorical(name="lmbda", choices=[10**x for x in range(-4, 4)]),
         "temperature": 1e-5,
         "num_lambda": 400,
-        "num_p_nm1": 128,
-        "sample_p_nm1_batch_size": 64,
-        "vol_samples": 64,
+        "num_p_nm1": 128 // x,
+        "sample_p_nm1_batch_size": 64 // x,
+        "vol_samples": 64 // x,
         "adjoint_matching": {
             "num_iterations": 10,
-            "batch_size": 64,
+            "batch_size": 64 // x,
             "clip_grad_norm": 2.0,
             "clip_loss": 1e5,
             "lr": trial.suggest_categorical(name="lr", choices=[5e-5, 1e-4, 1e-3]),
             "sampling": {
-                "num_samples": 64,
+                "num_samples": 64 // x,
                 "num_integration_steps": 40
             }
         },
